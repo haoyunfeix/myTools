32c7e538796b94f5af11c261f36e2aae624f9cf5
yunfeix.hao <yunfeix.hao@intel.com>
2015-02-17 11:03:41 +0800

    Sync work 20150217.1 Signed-off-by: yunfeix.hao <yunfeix.hao@intel.com>

---
+++ nightly/init_dir.sh
@@ -0,0 +1,6 @@
+if [[ $# -ne 1 ]];then
+    echo "The argument is not equal to 1, Please check..."
+    exit 1
+fi
+DIRNAME=$1
+mkdir -p $1/{cordova/{code,merge_xml,orig_xml/webdriver_xml,return_xml/webdriver_xml},wrt/{code,merge_xml,orig_xml/webdriver_xml,return_xml/webdriver_xml},webapi/{code,merge_xml,orig_xml/webdriver_xml,return_xml/webdriver_xml}}
